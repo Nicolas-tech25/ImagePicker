b4a1fdfe79bad1965a8ccb235d57f4a925c4b84b
@Nicolas_Oliveira <125362779+Nicolas-tech25@users.noreply.github.com>
2024-03-22 11:01:25 -0300

    aaa

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import { StatusBar } from "expo-status-bar";
-import { Button, View, Image, Text } from "react-native";
+import { Button, View, Image, Text, Share } from "react-native";
 import { useState, useEffect } from "react";
 import * as ImagePicker from "expo-image-picker";
+import * as MediaLibrary from "expo-media-library";
 
 export default function App() {
   //State tradicional para armazenarf a referência da foto (quando existir)
@@ -29,16 +30,37 @@ export default function App() {
     }
   };
   console.log(foto);
+
+  const acessarCamera = async () => {
+    const imagem = await ImagePicker.launchCameraAsync({
+      allowsEditing: false,
+      aspect: [16, 9],
+      quality: 0.5,
+    });
+    if (!imagem.canceled) {
+      await MediaLibrary.saveToLibraryAsync(imagem.assets[0].uri);
+      setFoto(imagem.assets[0].uri);
+    }
+  };
+
+  const compartilharFoto = () => {
+    Share.share({
+      message: "Verifique a imagem!",
+      url: foto.uri,
+    });
+  };
   return (
     <>
       <StatusBar />
       <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
         <Button onPress={escolherFoto} title="Escolher foto" />
-        <Image style={{ width: 300, height: 300 }} />
-        {foto ? (
-          <Image source={{ uri: foto }} style={{ width: 300, height: 300 }} />
-        ) : (
-          <Text>Você ainda não escolheu uma foto!</Text>
+        <Button onPress={acessarCamera} title="Tirar uma nova foto" />
+
+        {foto && (
+          <View>
+            <Image source={{ uri: foto }} style={{ width: 300, height: 300 }} />
+            <Button onPress={compartilharFoto} title="Compartilhar foto" />
+          </View>
         )}
       </View>
     </>
